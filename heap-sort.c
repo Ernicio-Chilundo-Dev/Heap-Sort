7ba927ec4c209bba1af5f37bf3ecf2b392961393
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-08-06 20:05:50 +0200

    - Added heapify() to maintain max heap property recursively - Implemented heapSort() to build max heap and sort elements - Integrated swap for node exchanges during heap operations

--- heap-sort.c
+++ heap-sort.c
@@ -10,3 +10,40 @@ void swap(int *a, int *b)
 }
 
 // Heapify function to maintain heap property
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    //If left child is larger than root
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    // If Right child larger than largest
+    if (right  < n && arr[right] > arr[largest])
+        largest = right;
+
+    // If largest is not root
+    if (largest != i)
+    {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
+    }
+
+}
+
+// Main function to perform heap sort
+void heapSort(int arr[], int n)
+{
+    // Build max heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // Extract elements from heap one by one
+    for (int i = n - 1; i > 0; i--)
+    {
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+}
